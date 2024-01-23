9a3848c022fbe9903562ea9009f6c5463fbdc5f0
nullun <git@nullun.com>
2024-01-23 10:22:56 +0000

    Client run-through

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import algosdk from 'algosdk';
+import * as algokit from "@algorandfoundation/algokit-utils";
 import { MsigAppClient } from "./client/MsigApp.client";
 
+const algod = new algosdk.Algodv2('a'.repeat(64), 'http://127.0.0.1', 4001);
 const kmd = new algosdk.Kmd('a'.repeat(64), 'http://127.0.0.1', 4002);
+
 const wallets = await kmd.listWallets();
 const walletId = wallets.wallets[0].id;
 const handle = await kmd.initWalletHandle(walletId, '');
@@ -13,8 +16,6 @@ for (const address of addresses) {
         sk: (await kmd.exportKey(handle.wallet_handle_token, '', address))['private_key']
     });
 }
-
-const algod = new algosdk.Algodv2('a'.repeat(64), 'http://127.0.0.1', 4001);
 
 const appClient = new MsigAppClient({
     resolveBy: 'id',
@@ -40,14 +41,11 @@ const msig_addr = algosdk.multisigAddress({
 });
 console.log(msig_addr);
 
-const sp = await algod.getTransactionParams().do();
-sp.flatFee = true;
-sp.fee = 1000;
 const init_mbr = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
     from: accounts[0].addr,
     to: deployment.appAddress,
     amount: 100000,
-    suggestedParams: sp
+    suggestedParams: await algod.getTransactionParams().do()
 });
 await algod.sendRawTransaction(init_mbr.signTxn(accounts[0].sk)).do();
 
@@ -58,14 +56,20 @@ const new_transaction_group = await appClient.arc55NewTransactionGroup({}, {
 const transaction_group = new_transaction_group.return as bigint;
 
 // Add Transaction
+const atc = new algosdk.AtomicTransactionComposer();
+const sim = new algosdk.modelsv2.SimulateRequest({
+    txnGroups: [],
+    allowUnnamedResources: true,
+    allowEmptySignatures: true
+});
 const zero_payment = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
     from: msig_addr,
     to: msig_addr,
     amount: 0,
-    suggestedParams: sp,
+    suggestedParams: await algod.getTransactionParams().do(),
     note: new Uint8Array(Buffer.from("Testing ARC-55 works"))
 });
-const txn_cost = await appClient.arc55MbrTxnIncrease({
+let txn_cost = await appClient.arc55MbrTxnIncrease({
     transaction: zero_payment.toByte()
 }, {
     sender: accounts[0]
@@ -74,7 +78,7 @@ const add_txn_mbr = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
     from: accounts[0].addr,
     to: deployment.appAddress,
     amount: txn_cost.return as bigint,
-    suggestedParams: sp
+    suggestedParams: await algod.getTransactionParams().do()
 });
 const add_txn = await appClient.arc55AddTransaction({
     costs: add_txn_mbr,
@@ -82,5 +86,64 @@ const add_txn = await appClient.arc55AddTransaction({
     index: 0,
     transaction: zero_payment.toByte()
 }, {
+    sender: accounts[0],
+    sendParams: {
+        populateAppCallResources: true,
+        suppressLog: true
+    }
+});
+
+// Set Signatures
+const sig_data = new Uint8Array(zero_payment.rawSignTxn(accounts[0].sk));
+txn_cost = await appClient.arc55MbrSigIncrease({
+    signatures: [sig_data]
+}, {
     sender: accounts[0]
+});
+const set_sig_mbr = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    from: accounts[0].addr,
+    to: deployment.appAddress,
+    amount: txn_cost.return as bigint,
+    suggestedParams: await algod.getTransactionParams().do()
+});
+const set_sig = await appClient.arc55SetSignatures({
+    costs: set_sig_mbr,
+    transactionGroup: transaction_group,
+    signatures: [sig_data]
+}, {
+    sender: accounts[0],
+    sendParams: {
+        populateAppCallResources: true,
+        suppressLog: true
+    }
+});
+
+// Clear Signatures
+const clear_sig = await appClient.arc55ClearSignatures({
+    transactionGroup: transaction_group,
+    address: accounts[0].addr
+}, {
+    sender: accounts[0],
+    sendParams: {
+        fee: algokit.microAlgos(2000)
+    }
+});
+
+// Remove Transaction
+const remove_txn = await appClient.arc55RemoveTransaction({
+    transactionGroup: transaction_group,
+    index: 0
+}, {
+    sender: accounts[0],
+    sendParams: {
+        fee: algokit.microAlgos(2000)
+    }
+});
+
+// Destroy
+const destroy_txn = await appClient.delete.destroy({}, {
+    sender: accounts[0],
+    sendParams: {
+        fee: algokit.microAlgos(2000)
+    }
 });
